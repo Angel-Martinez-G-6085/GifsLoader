2794bd666b7abdd7cc9f1ce5deae7e4f7f209f8c
Martinez Garrido Angel Ivan <mgangelivan@gmail.com>
2022-01-17 17:20:31 -0600

    componente y estilos de addCategory agregados

---
+++ src/components/AddCategory/AddCategory.jsx
@@ -0,0 +1,16 @@
+import './AddCategory.scss';
+import React from 'react';
+import { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+const AddCategory = (props) => {
+  return (
+    <Fragment>
+      <input type={'text'} />
+    </Fragment>
+  );
+};
+
+AddCategory.propTypes = {};
+
+export default AddCategory;
