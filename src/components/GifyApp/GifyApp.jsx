a5490ca3dda2b22cfbb7cec5a9f3309b65068165
Martinez Garrido Angel Ivan <mgangelivan@gmail.com>
2022-01-17 13:56:49 -0600

    componente GiffyApp y estilos del mismo creados

---
+++ src/components/GifyApp/GifyApp.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Fragment } from 'react';
+
+const GifyApp = (props) => {
+  return (
+    <Fragment>
+      <h2>GiffyApp</h2>
+      <hr />
+    </Fragment>
+  );
+};
+
+GifyApp.propTypes = {};
+
+export default GifyApp;
