0a3e05ed12d1fd85cce60fc3d66827c40c25dc89
Martinez Garrido Angel Ivan <mgangelivan@gmail.com>
2022-01-18 16:27:49 -0600

    componente GifGrid terminado

---
+++ src/components/GifGrid/GifGrid.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Fragment, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import fetchImages from '../../utils/fetchImages';
+
+const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetchImages(category, setImages);
+  }, []);
+
+  return (
+    <Fragment>
+      <div className="container images-container">
+        <h3>{category}</h3>
+        <div className="card-container">
+          {images.map((obj) => {
+            return (
+              <li key={obj.id}>
+                <img src={obj.url} />
+                <h3>{obj.title}</h3>
+              </li>
+            );
+          })}
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
+};
+
+export default GifGrid;
