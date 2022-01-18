c85cd4d8f6801eb6f0d4180de3ce9d620307be36
Martinez Garrido Angel Ivan <mgangelivan@gmail.com>
2022-01-18 16:30:50 -0600

    funcion para obtener las imagenes terminada

---
+++ src/utils/fetchImages.js
@@ -0,0 +1,25 @@
+const key = 'cNMT4MJPPqtprZmCrf6JHcBiZzTnIlYp';
+const axios = require('axios').default;
+
+const fetchImages = async (category, func) => {
+  const instance = axios.create({
+    baseURL: `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${category}&limit=5`,
+  });
+  try {
+    const response = await instance.get();
+    const { data } = response;
+    const finalData = data.data;
+    const filteredData = finalData.map((obj) => {
+      return {
+        id: obj.id,
+        title: obj.title,
+        url: obj.images.downsized_medium.url,
+      };
+    });
+    func(filteredData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default fetchImages;
