ac91deccd1ef394ddb5de9284d0720f1ef8198cf
Martinez Garrido Angel Ivan <mgangelivan@gmail.com>
2022-01-17 18:56:32 -0600

    componente y estilos de addCategory terminados

--- src/components/AddCategory/AddCategory.jsx
+++ src/components/AddCategory/AddCategory.jsx
@@ -1,16 +1,39 @@
 import './AddCategory.scss';
-import React from 'react';
-import { Fragment } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const AddCategory = (props) => {
+const AddCategory = ({ setCategories }) => {
+  const [value, setValue] = useState('');
+
+  // Handler
+  const inputHandler = (e) => {
+    return setValue(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (value.trim().length > 2) {
+      setCategories((oldState) => {
+        return [value, ...oldState];
+      });
+      setValue('');
+    }
+  };
+
   return (
-    <Fragment>
-      <input type={'text'} />
-    </Fragment>
+    <form onSubmit={submitHandler} className="input-container">
+      <input
+        className="category-input"
+        type={'text'}
+        value={value}
+        onChange={inputHandler}
+      />
+    </form>
   );
 };
 
-AddCategory.propTypes = {};
+AddCategory.propTypes = {
+  setCategories: PropTypes.func.isRequired,
+};
 
 export default AddCategory;
