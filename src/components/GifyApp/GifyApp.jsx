75d8034487e53d910340e0b343991594602c273f
Martinez Garrido Angel Ivan <mgangelivan@gmail.com>
2022-01-18 19:06:58 -0600

    componente y estilos del componente GifApp terminados

--- src/components/GifyApp/GifyApp.jsx
+++ src/components/GifyApp/GifyApp.jsx
@@ -1,12 +1,24 @@
+import './GifyApp.scss';
 import React from 'react';
+import { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Fragment } from 'react';
+import AddCategory from '../AddCategory/AddCategory.jsx';
+import GifGrid from '../GifGrid/GifGrid.jsx';
 
 const GifyApp = (props) => {
+  const [categories, setCategories] = useState([]);
+
   return (
     <Fragment>
-      <h2>GiffyApp</h2>
-      <hr />
+      <div className="container giffyapp-container">
+        <h2 className="header-title">Giffy Browser</h2>
+      </div>
+      <AddCategory setCategories={setCategories} />
+      <ol>
+        {categories.map((category) => {
+          return <GifGrid key={category} category={category} />;
+        })}
+      </ol>
     </Fragment>
   );
 };
